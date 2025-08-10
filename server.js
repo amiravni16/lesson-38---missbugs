f6b5fda3dadb118c17e324702124f00d2dda9285
amiravni16 <amiravni13@gmail.com>
2025-08-10 17:09:05 +0300

    Add Cookies and usage limit for them

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
@@ -7,6 +8,7 @@ const app = express()
 
 // App Configuration
 app.use(express.static('public'))
+app.use(cookieParser())
 
 // Basic - Routing in express
 app.get('/', (req, res) => res.send('Hello Muki'))
@@ -43,13 +45,34 @@ app.get('/api/bug/save', (req, res) => {
         })
         .catch((err) => {
             loggerService.error('Cannot save bug', err)
-            res.status(400).send('Cannot save bug')
+            res.status(400).send('Cannot get bug')
         })
 })
 
 // Read - getById
 app.get('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
+    
+    // Get visited bugs from cookie
+    let visitedBugs = req.cookies.visitedBugs ? JSON.parse(req.cookies.visitedBugs) : []
+    
+    // Check if user has already visited 3 different bugs
+    if (visitedBugs.length >= 3) {
+        return res.status(401).send('Wait for a bit')
+    }
+    
+    // Add current bug to visited list if not already visited
+    if (!visitedBugs.includes(bugId)) {
+        visitedBugs.push(bugId)
+        
+        // Log visited bugs to console
+        console.log('User visited at the following bugs:', visitedBugs)
+        
+        // Set cookie with 7 seconds expiration
+        res.cookie('visitedBugs', JSON.stringify(visitedBugs), {
+            maxAge: 7 * 1000 // 7 seconds in milliseconds
+        })
+    }
 
     bugService.getById(bugId)
         .then(bug => res.send(bug))
