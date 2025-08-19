3263e259706e57f678a43b54be8ad34ecf657185
amiravni16 <amiravni13@gmail.com>
2025-08-19 17:22:57 +0300

    Create user details component

---
+++ public/pages/UserDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BugList } from '../cmps/BugList.jsx';
+import { userService } from '../services/user.service.js';
+
+export function UserDetails() {
+    const user = userService.getLoggedinUser();
+    const userBugs = []; // Fetch or filter bugs created by the user
+
+    return (
+        <section className="user-details">
+            <h2>{user.username}'s Profile</h2>
+            <h3>Your Bugs</h3>
+            <BugList bugs={userBugs} onRemoveBug={() => {}} onEditBug={() => {}} />
+        </section>
+    );
+}
